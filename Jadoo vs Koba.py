ea93d6c836870dfb2309625b5678195cdd72f9d4
sakshi2199 <sakshi.sharma3099@gmail.com>
2020-10-06 16:16:29 +0530

    Create Jadoo vs Koba.py

---
+++ Jadoo vs Koba.py
@@ -0,0 +1,13 @@
+'''
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+'''
+
+# Write your code here
+
+for i in range(ord('F'),ord('Q')):
+    print(i)
